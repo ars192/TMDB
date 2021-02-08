fa96756d05b7ebf2f7f7f01e6b401753a5da43ee
ars192 <amanzholov.arystan@gmail.com>
2021-02-08 23:57:00 +0600

    Initial Commit

---
+++ TMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by Arystan on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
